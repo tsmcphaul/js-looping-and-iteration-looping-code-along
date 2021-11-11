71f4ea41f3e25d7c1cba583c36677b6621702560
Tammarra McPhaul <shaemc22@gmail.com>
2021-11-10 19:21:30 -0500

    commit tree 4a9342283a1ebb5bfa7be1e260352257e560bbd5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+// let names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+    for(let i=0; i < names.length; i++) {
+        names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return names;
+}
+
+function countDown(number){
+
+    while (number > -1) {
+        console.log(number);
+        number--;
+    }
+}
